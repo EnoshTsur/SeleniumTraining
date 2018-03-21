78314a3b589bf59402f9458680b585f00286d5e8
Enosh Tsur <enoshh12@gmail.com>
2018-03-21 17:21:16 +0200

    Enosh Tsur - Last change - page object

--- main/java/pages/TasksPage.java
+++ main/java/pages/TasksPage.java
@@ -1,15 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import lombok.Getter;
 import lombok.ToString;
 @ToString
 public class TasksPage extends AbstractPage {
 
-	// Page attributes
-	@Getter
-	private ToDoTasksWidget todo;
+	// Tasks page attributes
+	private final static By TODO = By.linkText("ToDo tasks");
 	
 	/***
 	 * CTR contains WebDriver
@@ -17,9 +17,16 @@ public class TasksPage extends AbstractPage {
 	 */
 	public TasksPage(WebDriver driver) {
 		super(driver);
-		todo = new ToDoTasksWidget(driver);
 	}
 
-
+	/***
+	 * Click on ToDoTasksWidget 
+	 * @return new ToDoTasksWidget page
+	 */
+	public ToDoTasksWidget clickOnToDo() {
+		wait.until(ExpectedConditions.elementToBeClickable(TODO));
+		bot.clickOn(TODO);
+		return new ToDoTasksWidget(driver);
+	}
 
 }
